7d14b3e6b4e4b3b3c7f95a04b289767f5d6a2cdf
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-03-10 00:59:43 -0600

    ISCLAB

--- vue-project/src/components/pregunta.vue
+++ vue-project/src/components/pregunta.vue
@@ -1,5 +1,18 @@
 <template>
-
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="gm.jpg" alt="Sunset in the mountains">
+  <div class="px-2 py-1">
+    <div class="font-red text-xl mb-1">Todos somos TECNM</div>
+    <p class="text-blue-100 text-base">
+      Los laboratorios son todos tuyos
+    </p>
+  </div>
+  <div class="px-2 pt-1 pb-0">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-100 mr-2 mb-2">#Motul</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ISC</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#FELINOS</span>
+  </div>
+</div>
 
 <left>
 <font size="30" color=#010055>¿Qué hacemos en isclab?</font>
